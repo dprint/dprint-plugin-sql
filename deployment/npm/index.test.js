dc8d4f940de1fba9cb6b5eab400f567156744436
David Sherret <dsherret@gmail.com>
2023-07-18 12:15:59 -0400

    Fix npm test

--- deployment/npm/index.test.js
+++ deployment/npm/index.test.js
@@ -6,4 +6,4 @@ const getBuffer = require("./index").getBuffer;
 const formatter = createFromBuffer(getBuffer());
 const result = formatter.formatText("file.sql", "SELECT  *  FROM dbo.Test");
 
-assert.strictEqual(result, "SELECT\n    *\nFROM\n    dbo.Test\n");
+assert.strictEqual(result, "SELECT\n  *\nFROM\n  dbo.Test\n");
